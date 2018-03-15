294a7aee7cb829de1d823a0ce8b1754bb07bdc8e
maple3142 <kirby741852963@gmail.com>
2018-03-15 20:42:33 +0800

    fix build for webpack 4

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,107 +1,31 @@
-var path = require('path')
-var webpack = require('webpack')
-var HtmlWebpackPlugin = require('html-webpack-plugin')
-var ExtractTextPlugin = require("extract-text-webpack-plugin")
-var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-var CopyWebpackPlugin = require('copy-webpack-plugin')
+const cfg = require('./webpack.config.base')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-var extcss = ExtractTextPlugin.extract({
-	fallback: 'style-loader',
-	use: 'css-loader?sourceMap'
-})
-
-module.exports = {
-	entry: {
-		app: ['whatwg-fetch', 'babel-polyfill', './src/index.js']
-	},
-	output: {
-		path: __dirname + '/dist',
-		filename: 'bundle.js'
-	},
-	module: {
-		loaders: [
-			{
-				loader: 'eslint-loader',
-				enforce: 'pre',
-				test: /(\.js$|\.vue$)/,
-				exclude: /node_modules/
-			},
-			{
-				loader: 'babel-loader',
-				test: /\.js$/,
-				exclude: /node_modules/
-			},
-			{
-				loader: 'vue-loader',
-				test: /\.vue$/,
-				options: {
-					loaders: {
-						css: extcss
-					}
-				}
-			},
-			{
-				test: /\.css/,
-				use: extcss
-			},
-			{
-				test: /\.(png|woff|woff2|eot|ttf|svg|jpg)$/,
-				loader: 'url-loader?limit=10000000'
-			}
-		]
-	},
-	resolve: {
-		alias: {
-			vue: 'vue/dist/vue.runtime.esm.js',
-			'@': path.resolve('./src')
-		},
-		extensions: ['*', '.js', '.vue', '.css']
-	},
-	plugins: [
-		new webpack.DefinePlugin({
-			'process.env': {
-				NODE_ENV: '"development"'
-			}
-		}),
-		new webpack.LoaderOptionsPlugin({
-			options: {
-				eslint: {
-					configFile: '.eslintrc'
-				}
-			}
-		}),
-		new HtmlWebpackPlugin({
-			title: 'WynnStats',
-			filename: 'index.html',
-			template: 'src/index.ejs',
-			inject: false
-		}),
-		new ExtractTextPlugin({
-			filename: 'style.css',
-			allChunks: true
-		}),
-		new CopyWebpackPlugin([{
-			from: './src/404.html',
-			to: '404.html'
-		}, {
-			from: './src/manifest.json',
-			to: 'manifest.json'
-		}, {
-			from: './src/favicon.ico',
-			to: 'favicon.ico'
-		}, {
-			from: './src/appicon.png',
-			to: 'appicon.png'
-		}])
-	],
-	devServer: {
-		port: process.env.PORT || 3000,
-		contentBase: './dist',
-		inline: true,
-		stats: 'errors-only',
-		historyApiFallback: {
-			index: 'index.html'
+cfg.plugins.push(
+	new webpack.DefinePlugin({
+		'process.env': {
+			NODE_ENV: '"development"'
 		}
-	},
-	devtool: 'source-map'
+	})
+)
+cfg.plugins.push(
+	new HtmlWebpackPlugin({
+		title: 'WynnStats',
+		filename: 'index.html',
+		template: 'src/index.ejs',
+		inject: false
+	})
+)
+cfg.devServer = {
+	port: process.env.PORT || 3000,
+	contentBase: './dist',
+	inline: true,
+	stats: 'errors-only',
+	historyApiFallback: {
+		index: 'index.html'
+	}
 }
+cfg.devtool = 'source-map'
+
+module.exports = cfg
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,126 +1,43 @@
-var path = require('path')
-var webpack = require('webpack')
-var HtmlWebpackPlugin = require('html-webpack-plugin')
-var ExtractTextPlugin = require("extract-text-webpack-plugin")
-var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-var CopyWebpackPlugin = require('copy-webpack-plugin')
+const cfg = require('./webpack.config.base')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
-var extcss = ExtractTextPlugin.extract({
-	fallback: 'style-loader',
-	use: 'css-loader?minimize&sourceMap'
-})
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
-module.exports = {
-	entry: {
-		app: ['whatwg-fetch', 'babel-polyfill', './src/index.js']
-	},
-	output: {
-		path: __dirname + '/dist',
-		filename: 'bundle.js'
-	},
-	module: {
-		loaders: [
-			{
-				loader: 'eslint-loader',
-				enforce: 'pre',
-				test: /(\.js$|\.vue$)/,
-				exclude: /node_modules/
-			},
-			{
-				loader: 'babel-loader',
-				test: /\.js$/,
-				exclude: /node_modules/
-			},
-			{
-				loader: 'vue-loader',
-				test: /\.vue$/,
-				options: {
-					loaders: {
-						css: extcss
-					}
-				}
-			},
-			{
-				test: /\.css/,
-				use: extcss
-			},
-			{
-				test: /\.(png|woff|woff2|eot|ttf|svg|jpg)$/,
-				loader: 'url-loader?limit=100000'
-			}
-		]
-	},
-	resolve: {
-		alias: {
-			vue: 'vue/dist/vue.runtime.esm.js',
-			'@': path.resolve('./src')
-		},
-		extensions: ['*', '.js', '.vue', '.css']
-	},
-	plugins: [
-		new webpack.DefinePlugin({
-			'process.env': {
-				NODE_ENV: '"production"'
-			}
-		}),
-		new webpack.LoaderOptionsPlugin({
-			options: {
-				eslint: {
-					configFile: '.eslintrc'
-				}
-			}
-		}),
-		new HtmlWebpackPlugin({
-			title: 'WynnStats',
-			filename: 'index.html',
-			template: 'src/index.ejs',
-			inject: false,
-			minify: {
-				collapseWhitespace: true,
-				removeComments: true,
-				removeRedundantAttributes: true
-			}
-		}),
-		new ExtractTextPlugin({
-			filename: 'style.css',
-			allChunks: true
-		}),
-		new webpack.optimize.UglifyJsPlugin({
+cfg.plugins.push(
+	new webpack.DefinePlugin({
+		'process.env': {
+			NODE_ENV: '"production"'
+		}
+	})
+)
+cfg.plugins.push(
+	new HtmlWebpackPlugin({
+		title: 'WynnStats',
+		filename: 'index.html',
+		template: 'src/index.ejs',
+		inject: false
+	})
+)
+cfg.plugins.push(
+	new UglifyJsPlugin({
+		uglifyOptions: {
 			compress: {
 				warnings: false
 			},
 			comments: false
-		}),
-		new OptimizeCssAssetsPlugin({
-			cssProcessorOptions: {
-				safe: true,
-				discardComments: {
-					removeAll: true,
-				},
-			},
-		}),
-		new CopyWebpackPlugin([{
-			from: './src/404.html',
-			to: '404.html'
-		}, {
-			from: './src/manifest.json',
-			to: 'manifest.json'
-		}, {
-			from: './src/favicon.ico',
-			to: 'favicon.ico'
-		}, {
-			from: './src/appicon.png',
-			to: 'appicon.png'
-		}])
-	],
-	devServer: {
-		port: process.env.PORT || 3000,
-		contentBase: './dist',
-		inline: true,
-		stats: 'errors-only',
-		historyApiFallback: {
-			index: 'index.html'
 		}
-	},
-	devtool: 'source-map'
-}
+	})
+)
+cfg.plugins.push(
+	new OptimizeCssAssetsPlugin({
+		cssProcessorOptions: {
+			safe: true,
+			discardComments: {
+				removeAll: true
+			}
+		}
+	})
+)
+module.exports = cfg
