5d6210d7895384952f3889e05486933b7155c50f
maple3142 <kirby741852963@gmail.com>
2018-03-18 14:00:06 +0800

    try to use lazyload component

--- src/router.js
+++ src/router.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+
 Vue.use(VueRouter)
 
-import Home from './vue/Home'
-import Player from './vue/Player'
-import Guild from './vue/Guild'
-import LeaderBoard from './vue/LeaderBoard'
-import Search from './vue/Search'
-import OnlinePlayers from './vue/OnlinePlayers'
-import Item from './vue/Item'
-import About from './vue/About'
+const Home = import(/* webpackChunkName: "Home" */ '@/vue/Home').then(x=>x.default)
+const Player = import(/* webpackChunkName: "Player" */ '@/vue/Player').then(x=>x.default)
+const Guild = import(/* webpackChunkName: "Guild" */ '@/vue/Guild').then(x=>x.default)
+const LeaderBoard = import(/* webpackChunkName: "LeaderBoard" */ '@/vue/LeaderBoard').then(x=>x.default)
+const Search = import(/* webpackChunkName: "Search" */ '@/vue/Search').then(x=>x.default)
+const OnlinePlayers = import(/* webpackChunkName: "OnlinePlayers" */ '@/vue/OnlinePlayers').then(x=>x.default)
+const Item = import(/* webpackChunkName: "Item" */ '@/vue/Item').then(x=>x.default)
+const About = import(/* webpackChunkName: "About" */ '@/vue/About').then(x=>x.default)
 
 const routes = [
 	{ path: '/', component: Home },
