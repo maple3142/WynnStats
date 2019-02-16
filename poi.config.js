f7a1ec0270cf965b27c41649bb53c976ebe8572a
maple <kirby741852963@gmail.com>
2019-02-16 12:26:27 +0800

    Update poi.config.js

--- poi.config.js
+++ poi.config.js
@@ -10,5 +10,6 @@ module.exports = {
 	chainWebpack: config => {
 		config.resolve.alias.set('@', path.join(__dirname, './src'))
 		config.resolve.extensions.add('.js').add('.vue')
-	}
+	},
+	publicFolder: 'static'
 }
